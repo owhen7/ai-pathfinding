a2f1c30ca92e9c73ec7b643a071888e878b7e87e
owhen7 <99552887+owhen7@users.noreply.github.com>
2023-02-04 13:39:17 -0500

    rudimentary display function

--- generateLevels.py
+++ generateLevels.py
@@ -1,6 +1,6 @@
 #This script generates 50 different levels, of class type Level, which are 101 x 101 grids of blocks that either have terrain or do not.
 #The terrain needs to be generated according to a certain algorithm.
-#This code will theoretically be ran only one time.
+
 from level import Level
 import pickle
 from random import *
@@ -18,11 +18,6 @@ def pickle50Levels():
 def generateLevelTerrain(currentLevel): #TODO: Rewrite this function according to the project specsheet. @Owen
     print("TODO: Generate Terrain for each level in this function. Terrain should be indicated with a 1.")
     currentLevel.array = [[randint(0, 1) for x in range(101)] for y in range(101)]
-
-
-
-
-
 
 if __name__ == "__main__":
     pickle50Levels();
--- pathfinding.py
+++ pathfinding.py
@@ -1,5 +1,6 @@
 import pickle 
 from level import Level
+from matplotlib import pyplot as plt
 
 def unpickle50Levels(levels):    
     with open('levels.pickle', 'rb') as file:
@@ -7,29 +8,23 @@ def unpickle50Levels(levels):
             currentLevel: Level = pickle.load(file)
             levels.append(currentLevel)
 
-
 def loadSpecificLevel(levels, levelSelectNum):
     print('Loading level ', levelSelectNum, "...", sep='')
     return levels[levelSelectNum]
 
-def displayLevel():
-    #This function displays a single enviroment in a GUI.
-    #Obviously loadSpecificLevel must work before display level does.
-    print("This does nothing right now but it will print the current, working level eventually.")
-    
+def displayLevel(incomingLevel):
+    #There is probably a better way to print the map and the player out. This will do for now.
+    plt.imshow(incomingLevel.array)
+    plt.show()
+
 def main():
     levels = []
     unpickle50Levels(levels)
     
     levelSelectNum = input("What level from 0-49 should be loaded? ")
-    levelSelect = loadSpecificLevel(levels, int(levelSelectNum))
+    currentLevelSelect = loadSpecificLevel(levels, int(levelSelectNum))
 
-    levelSelect.describe_level()
-
-
-
-
-
+    displayLevel(currentLevelSelect)
 
 if __name__ == "__main__":
     main()
